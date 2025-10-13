d731be18bdcfd1eff9396a7cc15720ec7d1efa92
kylix <2738386186@qq.com>
2025-10-13 14:47:01 +0800

    连接数据库

--- src/main/java/com/example/robotdelivery/pojo/Order.java
+++ src/main/java/com/example/robotdelivery/pojo/Order.java
@@ -1,12 +1,37 @@
 package com.example.robotdelivery.pojo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table(name="tb_order")
 @Entity
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="order_id")
     private Integer orderId;
+    private String orderName;
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderName='" + orderName + '\'' +
+                '}';
+    }
 }
